a11c1f66e70e109578ca1e1c3017bbca95cb4c96
Goloyy7 <indragoldy105@gmail.com>
2024-10-30 09:37:37 +0700

    Add files via upload

--- koneksi.php
+++ koneksi.php
@@ -3,11 +3,10 @@
 $host="sql112.infinityfree.com";
 $user="if0_37293949";
 $password="kjMznWmPF3";
-$db="if0_37293949_crud";
+$db ="if0_37293949_crud";
 
 $kon = mysqli_connect($host,$user,$password,$db);
 if (!$kon){
-        die("Koneksi Gagal:".mysqli_connect_error());
-        
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
 ?>
